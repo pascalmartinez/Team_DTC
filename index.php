8c7942cba4f7e4455067803307d3279ba19fced6
Chris Gold <chris.ratsimba87@gmail.com>
2018-04-19 23:15:48 +0200

    squelette

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+          <?php include 'header.php' ?>
+        </head>
+
+        <body>
+
+          
+          <?php include 'footer.php' ?>
+        </body>
+    </html>
