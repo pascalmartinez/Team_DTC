c21561a4511b9ec9b5b0e25560a6780281d5ccfd
Pascal <pmartinez31140@gmail.com>
2018-04-20 02:00:03 +0200

    page intro

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
     <html>
         <head>
-          <?php include 'header.php' ?>
+          <?php include 'header.html' ?>
         </head>
 
         <body>
+            <section class="container">
+                <h1 >Team DTC présente
+                    <span class="txt-rotate"
+                            data-period="2000"
+                            data-rotate='[ " Fatal error!!!."]'>
+                    </span>
+                </h1>
+            </section>
 
-          
+            <div class="image_hero">
+                <img src="img/souris_hache.png" alt="souris">
+            </div>
+
+            <a href="#" class="myButton">Play Game!</a>
+
+            
+
           <?php include 'footer.php' ?>
         </body>
     </html>
